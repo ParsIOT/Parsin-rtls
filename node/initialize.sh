f3055db06ea649172a197a9f8e388eacfc5e1a79
M.A <maheshmatkhah.prg@gmail.com>
2017-08-22 11:41:55 +0000

    add sudo

--- node/initialize.sh
+++ node/initialize.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Update
-apt-get update
-apt-get install -y htop vim python3 python3-requests
+sudo apt-get update
+sudo apt-get install -y htop vim python3 python3-requests
 
 # Install wireshark and other basics using all defaults
-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy install wireless-tools firmware-atheros usbutils wireshark tshark hostapd
+sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy install wireless-tools usbutils wireshark tshark hostapd
 
 # Copy latest scan.py from the repo
 wget https://raw.githubusercontent.com/schollz/find-lf/master/node/scan.py -O scan.py
