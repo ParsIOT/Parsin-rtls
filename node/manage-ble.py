15bf0b2db3a687b0f45a1c92b05c6c78cdbcfa54
M.A <maheshmatkhah.prg@gmail.com>
2017-09-02 19:55:40 +0430

    management script for ble rtls cluster

--- node/manage-ble.py
+++ node/manage-ble.py
@@ -15,9 +15,102 @@ sudo ./manage-ble.py -g "ble-1-rtls"
 """
 import argparse
 import atexit
+import json
 import os
 import subprocess
 import sys
+import threading
+import urllib.parse as urlparse
+from urllib.parse import urlencode
+
+import requests
+
+ssh_command = "ssh -o ConnectTimeout=10 pi@%(address)s "
+
+
+class CommandThread(threading.Thread):
+	def __init__(self, configs, command, first):
+		threading.Thread.__init__(self)
+		self.isFirst = first
+		self.config = configs
+		self.command = command
+		self.output = ""
+
+	def run(self):
+		if self.command == "status":
+			foo, self.output = self.is_scan_running()
+			print(self.output)
+		elif self.command == "start":
+			self.start_scan()
+		elif self.command == "stop":
+			self.stop_scan()
+		elif self.command == "restart":
+			self.restart_scan()
+		elif self.command == "update":
+			self.update_scanner()
+		elif self.command == "reboot":
+			self.reboot_pi()
+		elif self.command == "shutdown":
+			self.shutdown_pi()
+		else:
+			if self.isFirst:
+				print_help()
+
+	def is_scan_running(self):
+		c = ssh_command + '"ps aux"'
+		res, code = run_command(c % {'address': self.config['address']})
+		is_running = 'btmon' in res and 'scan-ble.py' in res
+		return is_running, self.config['note'] + ":\tScan is running" if is_running else self.config['note'] + ":\tScan is NOT running"
+
+	def shutdown_pi(self):
+		c = ssh_command + '"sudo init 0"'
+		r, code = run_command(c % {'address': self.config['address']})
+
+	def reboot_pi(self):
+		c = ssh_command + '"sudo init 6"'
+		r, code = run_command(c % {'address': self.config['address']})
+
+	def start_scan(self):
+		if self.is_scan_running()[0]:
+			print(self.config['note'] + ":\tAlready running")
+			return
+		c = ssh_command + '"sudo hcitool -i %(interface)s lescan --duplicates > /dev/null | sudo btmon |/home/pi/rtls/scan-ble.py --group %(group)s --server %(server)s --time %(scantime)s > /dev/null &"'
+		r, code = run_command(c % {'address': self.config['address'],
+		                           'interface': self.config['interface'],
+		                           'group': self.config['group'],
+		                           'server': self.config['lfserver'],
+		                           'scantime': self.config['scantime']
+		                           })
+		if code == 255:
+			return
+		if self.is_scan_running()[0]:
+			print(self.config['note'] + ":\tScan Started!")
+		else:
+			print(self.config['note'] + ":\tUnable To Start Scan")
+
+	def stop_scan(self):
+		is_running, msg = self.is_scan_running()
+		if is_running:
+			c = ssh_command + '"sudo pkill -9 hcitool && sudo hciconfig hci0 down && sudo hciconfig hci0 up && sudo pkill -9 btmon && sudo pkill -9 scan-ble.py"'
+			r, code = run_command(c % {'address': self.config['address']})
+			if self.is_scan_running()[0]:
+				print(self.config['note'] + ":\tCould Not Stop Scan!")
+				return False
+			else:
+				print(self.config['note'] + ":\tStopped!")
+				return True
+		else:
+			print(msg)
+			return True
+
+	def restart_scan(self):
+		if self.stop_scan():
+			self.start_scan()
+
+	def update_scanner(self):
+		c = 'scp %(file)s pi@%(address)s:/home/pi/rtls/'
+		r, code = run_command(c % {'address': self.config['address'],
+		                           'file': os.path.dirname(os.path.abspath(__file__)) + '/scan-ble.py'})
 
 
 def is_scan_running():
@@ -25,6 +118,41 @@ def is_scan_running():
 	ps_stdout = ps_output.stdout.read().decode('utf-8')
 	isRunning = 'hcitool' in ps_stdout or 'btmon' in ps_stdout
 	return isRunning
+
+
+def run_command(command):
+	print("\n====================\n\t\tRUNNING:\n", command, "\n")
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	# p = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	text = p.stdout.read().decode('utf-8')
+	retcode = p.wait()
+	print("\n\t\tRESUALT:", retcode, "\n====================\n")
+	return text, retcode
+
+
+def print_help():
+	print("""
+	manage-ble.py COMMAND
+
+		status:
+			get the current status of all Raspberry Pis in the cluster
+		stop :
+			stops scanning in all Raspberry Pis in the cluster
+		start:
+			starts scanning in all Raspberry Pis in the cluster
+		restart:
+			stops and starts all Raspberry Pis in the cluster
+		update :
+			uploads the latest version of scan-ble.py to Raspberry Pis
+		track -g GROUP:
+			communicate with find-lf server to tell it to track
+			for group GROUP
+		learn -u USER -g GROUP -l LOCATION:
+			communicate with find-lf server to
+			tell it to perform learning in the specified location for user/group.
+
+
+	""")
 
 
 def start_scan(hci, group, server):
@@ -48,56 +176,133 @@ def start_scan(hci, group, server):
 		print("Scan is running")
 
 
-def main():
-	# Check if SUDO
-	# http://serverfault.com/questions/16767/check-admin-rights-inside-python-script
-	if os.getuid() != 0:
-		print("you must run sudo!")
+def getURL(url, params):
+	url_parts = list(urlparse.urlparse(url))
+	query = dict(urlparse.parse_qsl(url_parts[4]))
+	query.update(params)
+	url_parts[4] = urlencode(query)
+	try:
+		getURL = urlparse.urlunparse(url_parts)
+		r = requests.get(getURL, timeout=3)
+		return r.text
+	except:
+		e = sys.exc_info()[0]
+		return "Problem requesting"
+
+
+def main(args, config):
+	command = args.command.strip()
+	print("", command)
+
+	if command == "track":
+		response = getURL(config['lfserver'] + "/switch", {'group': config['group']})
+		print(response)
 		return
 
-	# Parse arguments
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-g", "--group", default="", help="group name")
-	parser.add_argument(
-		"-i",
-		"--interface",
-		default='hci0',
-		help="Interface to listen on - default hci0")
-	parser.add_argument(
-		"-t",
-		"--time",
-		default=10,
-		help="scanning time in seconds (default 10)")
-	parser.add_argument(
-		"-s",
-		"--server",
-		default="https://lf.internalpositioning.com",
-		help="send payload to this server")
-	parser.add_argument("-n", "--nodebug", action="store_true")
-	args = parser.parse_args()
+	elif command == "learn":
+		if config['user'] == "" or config['location'] == "":
+			print("Must include name and location! Use ./manage-ble.py -u USER -l LOCATION learn")
+			return
+		config['user'] = config['user'].replace(':', '').strip()
+		response = getURL(config['lfserver'] + "/switch", {'group': config['group'], 'user': config['user'], 'loc': config['location']})
+		print(response)
+		return
 
-	# Check arguments for group
-	if args.group == "":
-		print("Must specify group with -g")
-		sys.exit(-1)
+	threads = []
+	temp = {}
+	for pi in config['pis']:
+		pi['group'] = config['group']
+		pi['lfserver'] = config['lfserver']
+		pi['scantime'] = config['scantime']
+		threads.append(CommandThread(pi.copy(), command, len(threads) == 0))
 
-	# Startup scanning
-	print("Using server " + args.server)
-	print("Using group " + args.group)
-
-	while True:
-		print("\n\n\t======\tWHILE\t======\t\n\n")
-		start_scan(args.interface, args.group, args.server)
+	# Start new Threads
+	for thread in threads:
+		thread.start()
+	for thread in threads:
+		try:
+			thread.join()
+		except:
+			pass
 
 
 def exit_handler():
-	print("Exiting...stopping scan..")
-	os.system("pkill -9 tshark")
+	print("Exiting...")
 
 
 if __name__ == "__main__":
 	atexit.register(exit_handler)
-	print("================================")
-	print(sys.argv)
-	print("================================")
-	main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		"-c",
+		"--config",
+		type=str,
+		default="config-ble.json",
+		help="location to configuration file")
+	parser.add_argument(
+		"-l",
+		"--location",
+		type=str,
+		default="",
+		help="location to use, for learning")
+	parser.add_argument(
+		"-u",
+		"--user",
+		type=str,
+		default="",
+		help="user to use, for learning")
+	parser.add_argument(
+		"-g",
+		"--group",
+		type=str,
+		default="",
+		help="group to use")
+	parser.add_argument("command", type=str, default="", help="start stop restart status track learn update reboot shutdown")
+	args = parser.parse_args()
+
+	config = {}
+	if not os.path.exists(args.config):
+		pis = []
+		while True:
+			pi = input('Enter Raspberry Pi address (e.g. 192.168.0.2 Enter blank if no more): ')
+			if len(pi) == 0:
+				break
+			note = input('Enter a note for the Raspberry Pi (for you to remember): ')
+			wlan = input('Interface to use (default: hci0): ')
+			if len(wlan) == 0:
+				wlan = "hci0"
+			pis.append({"address": pi.strip(), "note": note.strip(), "interface": wlan.strip()})
+		if len(pis) == 0:
+			print("Must include at least one computer!")
+			sys.exit(-1)
+		config['pis'] = pis
+		config['lfserver'] = input(
+			'Enter lf address (default: lf.internalpositioning.com): ')
+		if len(config['lfserver']) == 0:
+			config['lfserver'] = 'https://lf.internalpositioning.com'
+		if 'http' not in config['lfserver']:
+			config['lfserver'] = "http://" + config['lfserver']
+		config['group'] = input('Enter a group: ')
+		if len(config['group']) == 0:
+			config['group'] = 'default'
+		config['scantime'] = input('Enter a scanning time (default 1 seconds): ')
+		if len(config['scantime']) == 0:
+			config['scantime'] = 1
+		try:
+			config['scantime'] = int(config['scantime'])
+		except:
+			config['scantime'] = 1
+
+		with open(args.config, 'w') as f:
+			f.write(json.dumps(config, indent=4))
+
+	config = json.load(open(args.config, 'r'))
+	if args.group != "":
+		config['group'] = args.group
+		with open(args.config, 'w') as f:
+			f.write(json.dumps(config, indent=4))
+
+	config['user'] = args.user
+	config['location'] = args.location
+
+	main(args, config)
