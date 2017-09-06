a38711d5426f0f1a2ef1d90827359565437b32a9
M.A <maheshmatkhah.prg@gmail.com>
2017-09-06 19:50:35 +0430

    improve code

--- node/scan-ble.py
+++ node/scan-ble.py
@@ -5,6 +5,14 @@
 sudo /usr/bin/hcitool -i hci0 lescan --duplicates > /dev/null | sudo /usr/bin/btmon | ./scan-ble.py --group ble-1-rtls --server https://lf.internalpositioning.com &
 sudo hcitool lescan --duplicates > /dev/null | sudo btmon | ./scan-ble.py --group ble-1-rtls --server http://192.168.0.95:8072 --time 3
 
+	-g, --group
+		group name
+
+	-s, --server    default: https://lf.internalpositioning.com
+		rtls server address, including port number
+
+	-t --time       default: 1 second
+		time interval for sending data to rtls server
 
 """
 
@@ -42,35 +50,6 @@ def exit_handler():
 	os.system("sudo pkill -9 hcitool")
 	os.system("sudo hciconfig hci0 down")
 	os.system("sudo hciconfig hci0 up")
-
-
-atexit.register(exit_handler)
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-g", "--group", default="", help="group name")
-parser.add_argument(
-	"-s",
-	"--server",
-	default="https://lf.internalpositioning.com",
-	help="send payload to this server")
-parser.add_argument(
-	"-t",
-	"--time",
-	default=3,
-	type=float,  # float int
-	help="scanning time in seconds (default 3)")
-
-args = parser.parse_args()
-
-# Check arguments for group
-if args.group == "":
-	print("Must specify group with -g")
-	sys.exit(-1)
-
-print(args)
-# Startup scanning
-print("Using server " + args.server)
-print("Using group " + args.group)
 
 
 def submit_to_server():
@@ -120,6 +99,9 @@ def submit_to_server():
 			print("\n\t\t==========================================")
 			fingerprints2.clear()
 	except Exception:
+		print("\t\t++++++++++++++++++++++++++++++++++++++++++\n")
+		print("\t\tCould not send data to server!")
+		print("\n\t\t++++++++++++++++++++++++++++++++++++++++++")
 		fingerprints2.clear()
 		pass
 
@@ -128,20 +110,45 @@ def submit_to_server():
 	t_thread.start()
 
 
+atexit.register(exit_handler)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--group", default="", help="group name")
+parser.add_argument(
+	"-s",
+	"--server",
+	default="https://lf.internalpositioning.com",
+	help="send payload to this server")
+parser.add_argument(
+	"-t",
+	"--time",
+	default=1,
+	type=float,  # float int
+	help="scanning time in seconds (default 3)")
+
+args = parser.parse_args()
+
+# Check arguments for group
+if args.group == "":
+	print("Must specify group with -g")
+	sys.exit(-1)
+
+print("Using server " + args.server)
+print("Using group " + args.group)
+
 t_thread = threading.Timer(args.time, submit_to_server)
 t_thread.daemon = True
 t_thread.start()
 
-mac_regex = r"(?:[\s]+Address: ((?:[\w:]{2,3}){6}))"
-rssi_regex = r"(?:[\s]+RSSI: )((?:-[\d]+)|(?:[\w]+))"
 temp = {}
+# Startup scanning
 try:
 	for line in sys.stdin:
-		mac = re.findall(mac_regex, line)
+		mac = re.findall(r"(?:[\s]+Address: ((?:[\w:]{2,3}){6}))", line)
 		if mac:
 			temp['mac'] = mac[0].lower()
 
-		rssi = re.findall(rssi_regex, line)
+		rssi = re.findall(r"(?:[\s]+RSSI: )((?:-[\d]+)|(?:[\w]+))", line)
 		if rssi:
 			temp['rssi'] = rssi[0]
 		else:
